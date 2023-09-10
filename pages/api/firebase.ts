9903d25bec2dac5c38a3443b066ff606013c0c2a
satyarrr <satya9c27@gmail.com>
2023-09-10 20:03:11 +0800

    update code

--- pages/api/firebase.ts
+++ pages/api/firebase.ts
@@ -1,18 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import { getFirestore } from "firebase/firestore"
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAIeH_gCKmeA7V0kCjZKkNu1oR0aOy_wz0",
-  authDomain: "expense-tracker-22cf2.firebaseapp.com",
-  projectId: "expense-tracker-22cf2",
-  storageBucket: "expense-tracker-22cf2.appspot.com",
-  messagingSenderId: "385726893353",
-  appId: "1:385726893353:web:85a8fa342851b4eecc3b38",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 }
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
